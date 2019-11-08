aa3d7f67d2c682b396e0c74565680fec85a5e979
makinoshi <masa.kinoshita02@gmail.com>
2019-11-08 16:59:55 +0900

    Remove comment

--- dev/semantic_ui_reagent/dev.cljs
+++ dev/semantic_ui_reagent/dev.cljs
@@ -69,19 +69,14 @@
    [v/Item]
    [v/Statistic]])
 
-;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
   (js/console.log "start")
   (r/render [main-component]
             (.getElementById js/document "app")))
 
 (defn ^:export init []
-  ;; init is called ONCE when the page loads
-  ;; this is called in the index.html and must be exported
-  ;; so it is available even in :advanced release builds
   (js/console.log "init")
   (start))
 
-;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
   (js/console.log "stop"))
