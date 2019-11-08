e123c28bf72ea10fd13227fdbc5e3d51f48455d0
makinoshi <masa.kinoshita02@gmail.com>
2019-11-08 17:55:51 +0900

    Fix description on project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject toyokumo/semantic-ui-reagent "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A ClojureScript library for using Semantic UI React with Reagent on shadow-cljs."
+  :url "https://github.com/toyokumo/semantic-ui-reagent"
   :license {:name "Apache, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :deploy-repositories [["releases" {:url "https://repo.clojars.org" :creds :gpg}]
